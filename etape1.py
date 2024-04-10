99cd3dbba59c1dbaa775f699b3c660a6afb12740
Noah172 <114427265+Noah172@users.noreply.github.com>
2024-04-10 18:38:43 +0200

    Premier commit

--- etape1.py
+++ etape1.py
@@ -1,1 +1,30 @@
-nanaa
+def profondeurDAbord(depart):
+    def testEtatBut(etat):
+        # Votre logique pour vérifier si 'etat' est un état but
+        pass
+
+    def filsEtat(etat):
+        # Votre logique pour générer et retourner tous les fils de 'etat'
+        pass
+
+    def pop(liste):
+        return liste.pop(0)
+
+    def ajouterTete(e, liste):
+        liste.insert(0, e)
+
+    enAttente=[depart]
+    vus=[]
+    trouve=False
+
+    while enAttente and not trouve:
+        prochain=pop(enAttente)
+        vus.append(prochain)
+        if testEtatBut(prochain):
+            trouve=True
+            return True, prochain
+        else:
+            for e in filsEtat(prochain):
+                if e not in vus:
+                    ajouterTete(e, enAttente)
+    return False, depart
