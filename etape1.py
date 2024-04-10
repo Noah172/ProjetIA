a441066fbe424565f5cf19c3458bc01356da3b50
islamu <islam.zitouni1@gmail.com>
2024-04-10 18:55:36 +0200

    Etape1

--- etape1.py
+++ etape1.py
@@ -1,30 +1,40 @@
-def profondeurDAbord(depart):
-    def testEtatBut(etat):
-        # Votre logique pour vérifier si 'etat' est un état but
-        pass
+def profondeur_d_abord(depart, test_etat_but, fils_etat):
+    en_attente = [depart]  # Pile des états à explorer (LIFO)
+    vus = set()  # Ensemble des états déjà explorés
 
-    def filsEtat(etat):
-        # Votre logique pour générer et retourner tous les fils de 'etat'
-        pass
+    while en_attente:  # Tant que la pile n'est pas vide
+        prochain = en_attente.pop()  # Retire le dernier élément ajouté à la pile
+        prochain_tuple = tuple(prochain)  # Convertit la liste en tuple pour la rendre hashable
 
-    def pop(liste):
-        return liste.pop(0)
+        if prochain_tuple in vus:
+            continue
+        vus.add(prochain_tuple)  # Ajoute l'état actuel (sous forme de tuple) aux états déjà vus
 
-    def ajouterTete(e, liste):
-        liste.insert(0, e)
+        if test_etat_but(prochain):
+            return True, prochain  # Retourne vrai et l'état but si trouvé
 
-    enAttente=[depart]
-    vus=[]
-    trouve=False
+        for e in fils_etat(prochain):
+            if tuple(e) not in vus:
+                en_attente.append(e)  # Ajoute les fils non explorés à la pile
 
-    while enAttente and not trouve:
-        prochain=pop(enAttente)
-        vus.append(prochain)
-        if testEtatBut(prochain):
-            trouve=True
-            return True, prochain
-        else:
-            for e in filsEtat(prochain):
-                if e not in vus:
-                    ajouterTete(e, enAttente)
-    return False, depart
+    return False, depart  # Retourne faux et l'état initial si aucun but n'est atteint
+
+def test_etat_but(etat):
+    # Vérifie si la liste des chiffres est triée
+    return etat == sorted(etat)
+
+def fils_etat(etat):
+    fils = []
+    for i in range(len(etat) - 1):
+        etat_fils = etat[:]  # Crée une copie de l'état actuel
+        etat_fils[i], etat_fils[i+1] = etat_fils[i+1], etat_fils[i]  # Echange les chiffres à la position i et i+1
+        fils.append(etat_fils)
+    return fils
+
+# Exemple d'utilisation
+etat_initial = [3, 1, 2]
+trouve, etat_final = profondeur_d_abord(etat_initial, test_etat_but, fils_etat)
+if trouve:
+    print("État but atteint :", etat_final)
+else:
+    print("État but non atteint à partir de l'état initial :", etat_initial)
