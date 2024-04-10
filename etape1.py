ad027d39c5ed8bd6acdf13fc5028182717264316
Noah172 <114427265+Noah172@users.noreply.github.com>
2024-04-10 19:21:11 +0200

    Premier commit

--- etape1.py
+++ etape1.py
@@ -1,40 +1,35 @@
-def profondeur_d_abord(depart, test_etat_but, fils_etat):
-    en_attente = [depart]  # Pile des états à explorer (LIFO)
-    vus = set()  # Ensemble des états déjà explorés
+def test_etat_but(etat):
+    return etat==sorted(etat)
 
-    while en_attente:  # Tant que la pile n'est pas vide
-        prochain = en_attente.pop()  # Retire le dernier élément ajouté à la pile
-        prochain_tuple = tuple(prochain)  # Convertit la liste en tuple pour la rendre hashable
+def fils_etat(etat):
+    return [etat[:i] + [etat[i+1], etat[i]] + etat[i+2:] for i in range(len(etat) - 1)]
+
+
+def profondeur_d_abord(depart, test_etat_but, fils_etat):
+    en_attente = [depart]  
+    vus = set()  
+    
+    while en_attente: 
+        prochain = en_attente.pop()
+        prochain_tuple = tuple(prochain) 
 
         if prochain_tuple in vus:
             continue
-        vus.add(prochain_tuple)  # Ajoute l'état actuel (sous forme de tuple) aux états déjà vus
+        vus.add(prochain_tuple)  
 
         if test_etat_but(prochain):
-            return True, prochain  # Retourne vrai et l'état but si trouvé
+            return True, prochain  
 
         for e in fils_etat(prochain):
             if tuple(e) not in vus:
-                en_attente.append(e)  # Ajoute les fils non explorés à la pile
+                en_attente.append(e)  
 
-    return False, depart  # Retourne faux et l'état initial si aucun but n'est atteint
+    return False, depart  
 
-def test_etat_but(etat):
-    # Vérifie si la liste des chiffres est triée
-    return etat == sorted(etat)
 
-def fils_etat(etat):
-    fils = []
-    for i in range(len(etat) - 1):
-        etat_fils = etat[:]  # Crée une copie de l'état actuel
-        etat_fils[i], etat_fils[i+1] = etat_fils[i+1], etat_fils[i]  # Echange les chiffres à la position i et i+1
-        fils.append(etat_fils)
-    return fils
 
-# Exemple d'utilisation
 etat_initial = [3, 1, 2]
 trouve, etat_final = profondeur_d_abord(etat_initial, test_etat_but, fils_etat)
-if trouve:
-    print("État but atteint :", etat_final)
-else:
-    print("État but non atteint à partir de l'état initial :", etat_initial)
+print("État but:", etat_final)
+print("fils etat",fils_etat(etat_initial))
+
