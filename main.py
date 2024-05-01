6d3916d962e3dcb7a796e079e1795e886951ca13
Noah172 <noah.lopez82@orange.fr>
2024-05-01 16:58:36 +0200

    (Noah) Etape 2 fini

--- main.py
+++ main.py
@@ -2,87 +2,58 @@ from Liste import *
 from Etat import *
 import time
 
-
+"""
 def Profondeur_dabord(etat_initial, but):
     enAttente=[etat_initial]
     vus=[]
     trouve=False
     while enAttente:
-        print(enAttente)
-        print(taille(enAttente))
         actuel=enAttente.pop(0)
         vus.append(actuel)
-        if actuel==but:
+        if est_but(actuel,but):
             trouve=True
             return (True,actuel)
         else:
-            for etat_fils in actuel.filsEtat():
-                if etat_fils not in vus:
+            for etat_fils in fils_etat(actuel):
+                print(etat_fils)
+                print(vus)
+                if not any(np.array_equal(etat_fils, vu) for vu in vus):
                     ajouterTete(etat_fils, enAttente)
     if not trouve:
         return (False,etat_initial)
+"""
 
-print("Début du test de la recherche en profondeur d'abord bornée...")
+def Profondeur_dabord_bornee(etat_initial, but,limite):
+    enAttente = [(etat_initial, 0)]
+    vus=[]
+    trouve=False
+    while enAttente:
+        actuel,profondeur=enAttente.pop(0)
+        vus.append(actuel)
+        if est_but(actuel,but):
+            trouve=True
+            return (True,actuel)
+        if profondeur< limite:
+            for etat_fils in fils_etat(actuel):
+                if not any(np.array_equal(etat_fils, vu) for vu in vus):
+                    ajouterTete((etat_fils, profondeur + 1), enAttente)
+    if not trouve:
+        return (False,etat_initial)
 
 etat_debut =[[0, 0, 0, 6],
      [5, 9, 0, 7],
      [1, 3, 0, 3]]
 
 
-etat_but = [[6, 2, 0, 0],
-     [2, 1, 0, 2],
-     [1, 3, 0, 3]]
+etat_but = [[0, 0, 0, 0],
+     [0, 9, 5, 7],
+     [1, 3, 6, 3]]
 
+
+print("ETAT DEBUT : ")
 afficher(etat_debut)
+print("ETAT FINAL : ")
+afficher(etat_but)
 
-
-liste_fils = fils_etat(etat_debut)
-afficher_liste_etat(liste_fils)
-
-"""
-trouve, etat_final = Profondeur_dabord(etat_debut, etat_but)
-
-if trouve:
-    print("Trouvé ")
-else:
-    print("Aucun état but n'a été trouvé.")
-
-print("État initial:")
-etat_debut.afficher()
-
-print("État atteint:")
-etat_final.afficher()
-
-print("État but:")
-etat_but.afficher()
-
-"""
-
-"""
-. 2 . 1
-2 1 . 2
-1 3 . 3
-
-Fils d'un Etats
-
-. 2 . .
-2 1 . 2
-1 3 1 3
-
-. . . 1
-2 1 . 2
-1 3 2 3
-
-. 2 . 1
-. 1 . 2
-1 3 2 3
-
-2 . . 1
-2 1 . 2
-1 3 . 3
-
-1 2 . .
-2 1 . 2
-1 3 . 3
-
-"""
+print("Début du test de la recherche en profondeur d'abord bornée...")
+print(Profondeur_dabord_bornee(etat_debut,etat_but,2))
